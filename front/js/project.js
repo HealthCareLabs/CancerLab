d864d077d7e7adff0afca9c1e9ac7ff40f333e31
Elena Musatova <e.musatova@innopolis.ru>
2016-04-28 23:14:32 +0300

    Dashboard charts and treatments

--- front/js/project.js
+++ front/js/project.js
@@ -1,3 +1,57 @@
 jQuery(document).ready(function ($) {
-
+    getNumStatistics();
+    getSizeStatistics();
+    getAgeStatistics();
+    getSexStatistics();
 });
+
+
+function getNumStatistics() {
+    var url = "js/json/treatment_number.json";
+
+    $.getJSON(url, function (data) {
+        new Chartist.Line('.chart_type_treatment-num', data['data']);
+    });
+}
+
+function getSizeStatistics() {
+    var url = "js/json/tumor_size.json";
+
+    $.getJSON(url, function (data) {
+        var nData = data['data'];
+        var sum = function(a, b) { return a + b };
+        new Chartist.Pie('.chart_type_tumor-size', nData, {
+            labelInterpolationFnc: function(value) {
+                return Math.round(value / nData.series.reduce(sum) * 100) + '%';
+            }
+        });
+    });
+}
+
+function getAgeStatistics() {
+    var url = "js/json/age.json";
+
+    $.getJSON(url, function (data) {
+        var nData = data['data'];
+        var sum = function(a, b) { return a + b };
+        new Chartist.Pie('.chart_type_age', nData, {
+            labelInterpolationFnc: function(value) {
+                return Math.round(value / nData.series.reduce(sum) * 100) + '%';
+            }
+        });
+    });
+}
+
+function getSexStatistics() {
+    var url = "js/json/sex.json";
+
+    $.getJSON(url, function (data) {
+        var nData = data['data'];
+        var sum = function(a, b) { return a + b };
+        new Chartist.Pie('.chart_type_sex', nData, {
+            labelInterpolationFnc: function(value) {
+                return Math.round(value / nData.series.reduce(sum) * 100) + '%';
+            }
+        });
+    });
+}
