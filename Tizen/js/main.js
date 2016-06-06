f204df8e74596ab40e9777e74ebdf21f71eeca8c
Elena Musatova <e.musatova@innopolis.ru>
2016-06-06 20:04:56 +0300

    Init tizen project and add some styles

---
+++ Tizen/js/main.js
@@ -0,0 +1,70 @@
+jQuery(document).ready(function ($) {
+    $('.form_action_register').initForm(function(result){
+        localStorage.setItem('ApiKey', result['ApiKey']);
+        window.location.href = "#main";
+    }, function(result) {
+        alert(result['Message']);
+    });
+
+    $('.form_action_main').initForm(function(result){
+        $('.user').text(result['PatientProfile']['LastName']);
+    }, function(result) {
+        alert(result['Message']);
+    });
+
+
+    $('.button_action_start').click(function() {
+        if(localStorage.getItem('ApiKey')) {
+            $('#main .text').text('API Key = ' + localStorage.getItem('ApiKey'));
+            window.location.href = "#main";
+        } else {
+            window.location.href = "#auth";
+        }
+    });
+
+    $('.button_action_clear').click(function() {
+        localStorage.clear();
+    });
+});
+
+
+$.fn.serializeObject = function() {
+    var o = {};
+    var a = this.serializeArray();
+    $.each(a, function() {
+        if (o[this.name] !== undefined) {
+            if (!o[this.name].push) {
+                o[this.name] = [o[this.name]];
+            }
+            o[this.name].push(this.value || '');
+        } else {
+            o[this.name] = this.value || '';
+        }
+    });
+    return o;
+};
+
+$.fn.initForm = function(successFunction, errorFunction) {
+    var frm = this;
+    frm.submit(function (ev) {
+        $.ajax({
+            type: frm.attr('method'),
+            url: frm.attr('action'),
+            contentType: 'application/json',
+            data: JSON.stringify(frm.serializeObject()),
+            dataType: 'json',
+            beforeSend: function(request){
+                localStorage.getItem('ApiKey') && request.setRequestHeader('X-ApiKey', localStorage.getItem('ApiKey'));
+            },
+            success: function (result) {
+                if(result['Success']) {
+                    successFunction(result);
+                } else {
+                    errorFunction(result);
+                }
+            }
+        });
+
+        ev.preventDefault();
+    });
+};
